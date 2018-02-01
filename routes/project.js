0b9c8c6feb603a2c13777718fa7d00cde4cf6b21
Tyler Tran <tyml.tran@gmail.com>
2018-02-01 00:50:14 -0800

    Updated to work as per lab4: node.js lab

--- routes/project.js
+++ routes/project.js
@@ -1,4 +1,8 @@
 // Makes a controller called 'viewProject' available to application
 exports.viewProject = function(req, res) { 
-  res.render("project.handlebars"); // Renders project page template
+  var name = req.params.name; 
+  console.log("The project name is: " + name);
+  res.render('project', { // Renders project page template
+    'projectName': name
+  });
 };
